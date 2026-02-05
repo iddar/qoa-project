bee0b38863c63f35c1bd3bc38ab365144c37a5df
Iddar Olivares <iddar@dbug.mx>
2026-02-05 16:03:48 -0600

    feat: integrate Logestic logging library into the application

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,8 @@
 import { Elysia } from 'elysia';
 
 import { cors } from '@elysiajs/cors';
+import { Logestic } from 'logestic';
+
 import { authModule } from '../modules/auth';
 import { healthModule } from '../modules/health';
 import { usersModule } from '../modules/users';
@@ -8,10 +10,11 @@ import { openApiPlugin } from './plugins/openapi';
 
 export const createApp = () =>
   new Elysia({ name: 'qoa-app', prefix: '/v1' })
+    .use(Logestic.preset('common'))
     .use(cors())
     .use(openApiPlugin)
     .use(healthModule)
     .use(authModule)
     .use(usersModule);
 
-    export type App = ReturnType<typeof createApp>;
+export type App = ReturnType<typeof createApp>;
